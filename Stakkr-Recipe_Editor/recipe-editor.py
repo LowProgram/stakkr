68d547ceb6ef83dcb2625baa7a5349b1810b0fbc
Jordi Wijsen <1646664wijsen@zuyd.nl>
2019-06-28 11:52:21 +0200

    Code clean up
    -Cleaned up the code some more
    +Made a beginning to service merger

--- Stakkr-Recipe_Editor/recipe-editor.py
+++ Stakkr-Recipe_Editor/recipe-editor.py
@@ -1,22 +1,20 @@
 
-go = False
-inputFiles = []
+
+
 print("Enter the components you want to use: ")
-while  go == False:
+while True:
+    inputFiles = []
     userFileChoice = input()
     if userFileChoice == "go":
-        go == True
+
         break
     else:
         inputFiles.append(userFileChoice)
 
 endFilename = input("Name your recipe: ")
-for file in inputFiles:
+with open(endFilename, "w+") as outputFile:
+    for filename in inputFiles:
+        with open(filename, 'a+') as inputFiles:
 
-
-#with open(endFilename, 'w') as outputFile:
-   #for filename in inputFiles:
-        #with open(filename, 'r') as inputFile:
-            #for service in seric
 
 
