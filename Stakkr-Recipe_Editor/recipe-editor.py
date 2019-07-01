198d66aecffc3c51ad5df688929493f6b194e3dd
Jordi Wijsen <1646664wijsen@zuyd.nl>
2019-07-01 11:55:59 +0200

    Added comments
    + Comments added to explain what things do
    + Tried making the part where every file gets merged 
    ! Issue with opening files in for loop

--- Stakkr-Recipe_Editor/recipe-editor.py
+++ Stakkr-Recipe_Editor/recipe-editor.py
@@ -1,20 +1,35 @@
-
-
-
-print("Enter the components you want to use: ")
+# User input; the files that need to be merged
+print('Enter the components you want to use: ')
 while True:
     inputFiles = []
     userFileChoice = input()
-    if userFileChoice == "go":
-
+    if userFileChoice == "":
         break
     else:
         inputFiles.append(userFileChoice)
 
-endFilename = input("Name your recipe: ")
-with open(endFilename, "w+") as outputFile:
-    for filename in inputFiles:
-        with open(filename, 'a+') as inputFiles:
+# Ask user what name the recipe is gonna be
+endFilename = input('Name your recipe: ')
+with open(endFilename, 'w') as outputFile:
+    # Should walk trough the inputFiles list opening and merging one file at the time
+    for i in inputFiles:
+        with open(inputFiles[i], 'r') as f:
+            readData = f.read
+            f.close()
+
+            # First file determines format
+            if i == inputFiles[0]:
+                outputFile.write(readData)
+
+            # Search data and merge this into the outputFile
+            else:
+                do = 'nothing'
+
+                    
+                    
+                    
+
+
 
 
 
