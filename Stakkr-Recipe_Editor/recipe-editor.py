4b89c8c602f63086f68693b0fbafcf3bff04f084
Jordi Wijsen <1646664wijsen@zuyd.nl>
2019-07-01 18:28:00 +0200

    Fix for opening files
    ~ Changed loop for requesting user input
    ~ Fix for opening the files; incorrect function

--- Stakkr-Recipe_Editor/recipe-editor.py
+++ Stakkr-Recipe_Editor/recipe-editor.py
@@ -1,33 +1,37 @@
+inputFiles = []
+headerlist = ['service', 'proxy', 'aliases', 'services', 'commands', 'messages']
+
 # User input; the files that need to be merged
 print('Enter the components you want to use: ')
-while True:
-    inputFiles = []
+userFileChoice = input()
+while userFileChoice != '':
+    inputFiles.append(userFileChoice)
     userFileChoice = input()
-    if userFileChoice == "":
-        break
-    else:
-        inputFiles.append(userFileChoice)
 
 # Ask user what name the recipe is gonna be
 endFilename = input('Name your recipe: ')
+
+def mergeData(readData : str):
+    for header in headerlist:
+        for i in readData[header]:
+            outputFile.write(readData[header])
+
 with open(endFilename, 'w') as outputFile:
     # Should walk trough the inputFiles list opening and merging one file at the time
     for i in inputFiles:
-        with open(inputFiles[i], 'r') as f:
-            readData = f.read
+        with open(i, 'r') as f:
+            readData = f.read()
             f.close()
-
             # First file determines format
             if i == inputFiles[0]:
                 outputFile.write(readData)
 
             # Search data and merge this into the outputFile
             else:
-                do = 'nothing'
+                mergeData(readData)
 
                     
-                    
-                    
+
 
 
 
