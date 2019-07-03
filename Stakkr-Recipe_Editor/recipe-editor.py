92f3a7b5d278e96824a1a22f8aa7b7ad143c8499
Jordi Wijsen <1646664wijsen@zuyd.nl>
2019-07-03 11:46:16 +0200

    Change to yml
    +Changed to yaml loading and dumping

--- Stakkr-Recipe_Editor/recipe-editor.py
+++ Stakkr-Recipe_Editor/recipe-editor.py
@@ -1,3 +1,5 @@
+import yaml
+
 input_list = []
 header_list = ['service', 'proxy', 'aliases', 'services', 'commands', 'messages']
 # User input; the files that need to be merged
@@ -10,21 +12,22 @@ while user_input != '':
 # Ask user what name the recipe is gonna be
 end_f = input('Name your recipe: ')
 
-def merge_data(f_data : str):
+def merge_data(f_dict : str):
     for header in header_list:
-        for i in f_data[header]:
-            output_f.write(f_data[header])
+        for i in f_dict[header]:
+           
 
 with open(end_f, 'w') as output_f:
     # Should walk trough the input_list list opening and merging one file at the time
     for i in input_list:
         with open(i, 'r') as f:
-            f_data = f.read()
+            f_dict = yaml.load(f)
             f.close()
             # First file determines format
             if i == input_list[0]:
+                f_data = yaml.dump(f_dict)
                 output_f.write(f_data)
 
             # Search data and merge this into the output_f
             else:
-                merge_data(f_data)
+                merge_data(f_dict)
