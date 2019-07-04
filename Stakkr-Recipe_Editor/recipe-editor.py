925a4d567488397cfc2c2817ffb338be1fcc64fd
Jordi Wijsen <1646664wijsen@zuyd.nl>
2019-07-04 10:35:17 +0200

    Error catch  file input_list
    + Error catch added that request change file when FileNotFound
    ~ Changed merge_data to merge dicts 
    ~ Code restructured
    ! BUG: Dicts merge not correctly

--- Stakkr-Recipe_Editor/recipe-editor.py
+++ Stakkr-Recipe_Editor/recipe-editor.py
@@ -1,9 +1,8 @@
 import yaml
 
 input_list = []
-header_list = ['service', 'proxy', 'aliases', 'services', 'commands', 'messages']
 # User input; the files that need to be merged
-print('Enter the components you want to use: ')
+print('Enter the components you want to use, press enter when complete: ')
 user_input = input()
 while user_input != '':
     input_list.append(user_input)
@@ -12,22 +11,39 @@ while user_input != '':
 # Ask user what name the recipe is gonna be
 end_f = input('Name your recipe: ')
 
-def merge_data(f_dict : str):
-    for header in header_list:
-        for i in f_dict[header]:
-           
+
+def merge_dict_list(*dict_list):
+    output_dict = {}
+    for d in dict_list:
+        for key in d:
+            try:
+                output_dict[key].append(d[key])
+            except KeyError:
+                output_dict[key] = [d[key]]
+    return output_dict
+
 
 with open(end_f, 'w') as output_f:
+    dict_list = []
     # Should walk trough the input_list list opening and merging one file at the time
     for i in input_list:
-        with open(i, 'r') as f:
-            f_dict = yaml.load(f)
-            f.close()
-            # First file determines format
-            if i == input_list[0]:
-                f_data = yaml.dump(f_dict)
-                output_f.write(f_data)
-
-            # Search data and merge this into the output_f
-            else:
-                merge_data(f_dict)
+        try:
+            with open(i, 'r') as f:
+                f_dict = yaml.load(f)
+                dict_list.append(f_dict)
+                f.close()
+        except FileNotFoundError:
+            print('\nFile "', i, '" not found')
+            change_f = input('Input the file again or press enter to remove: ')
+            input_list.remove(i)
+            if change_f != '':
+                input_list.insert(input_list.count(i), change_f) #LINE OVERBODIG?
+                print('\nSuccessfully changed to ', change_f)
+                with open(change_f, 'r') as f:
+                    f_dict = yaml.load(f)
+                    dict_list.append(f_dict)
+                    f.close()
+        print(f_dict)
+    f_data = yaml.dump(merge_dict_list(*dict_list))
+    output_f.write(f_data)
+    output_f.close()
