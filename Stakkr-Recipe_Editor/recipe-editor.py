5708f4fbd7340f4e5dcef3a420b46d4b071d8278
Jordi Wijsen <1646664wijsen@zuyd.nl>
2019-07-08 00:51:52 +0200

    Code restructured & fixes to recipe components
    ~ Fixed wrong spelled word in mysql.yml
    ~ Fixed incorrect indent in php.yml
    ~ Moved input request into def input_service

--- Stakkr-Recipe_Editor/recipe-editor.py
+++ Stakkr-Recipe_Editor/recipe-editor.py
@@ -2,8 +2,11 @@
 
 import yaml
 
+dict_list = []
+input_list = []
 
-def merge_dict_list(*dict_list):
+
+def merge_dicts(*dict_list):
     # Merges all dicts in dict_list into a new one
     output_dict = {}
     for d in dict_list:
@@ -18,10 +21,7 @@ def merge_dict_list(*dict_list):
 def add_dict_list(i):
     # Tries to add recipe component to dict_list
     try:
-        with open(i, 'r') as f:
-            f_dict = yaml.load(f)
-            dict_list.append(f_dict)
-            f.close()
+        load_service(i)
     except FileNotFoundError:
         print('\nFile "', i, '" not found\n')
         change_f = input('\nInput the file again or press enter to remove: ')
@@ -29,10 +29,7 @@ def add_dict_list(i):
         if change_f != '':
             input_list.insert(input_list.count(i), change_f)
             print('\nSuccessfully changed to "', change_f, '"')
-            with open(change_f, 'r') as f:
-                f_dict = yaml.load(f)
-                dict_list.append(f_dict)
-                f.close()
+            load_service(i)
 
 
 def recipe_create(input_list):
@@ -41,19 +38,32 @@ def recipe_create(input_list):
         for i in input_list:
             add_dict_list(i)
 
-        f_data = yaml.dump(merge_dict_list(*dict_list))
+        f_data = yaml.safe_dump(merge_dicts(*dict_list))
         output_f.write(f_data)
         output_f.close()
 
 
-dict_list = []
-input_list = []
-# User input; the files that need to be merged
+def load_service(i):
+    # Loads the service into a dict
+    with open(i, 'r') as f:
+        f_dict = yaml.safe_load(f)
+        print(f_dict)
+        dict_list.append(f_dict)
+        f.close()
+    return f_dict
+
+
+def input_services():
+    # User input; the files that need to be merged
+    service = input()
+    while service != '':
+        input_list.append(service)
+        service = input()
+    return input_list
+
+
 print('Enter the components you want to use, press enter when complete: ')
-user_input = input()
-while user_input != '':
-    input_list.append(user_input)
-    user_input = input()
+input_list = input_services()
 
 # Ask user what name the recipe is gonna be
 end_f = input('Name your recipe: ')
